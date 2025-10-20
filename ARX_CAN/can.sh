c5e6ac99a7b216949b87e4d3be1d120a00a6cfbb
zishangji-nuc8 <zishangji@qq.com>
2025-10-20 10:58:16 +0800

    删除没用的can口

--- ARX_CAN/can.sh
+++ ARX_CAN/can.sh
@@ -5,9 +5,4 @@ workspace=$(pwd)
 source ~/.bashrc
 
 
-gnome-terminal -t "can1" -x sudo bash -c "cd ${workspace}/arx_can; ./arx_can1.sh; exec bash;"
-sleep 0.1
-gnome-terminal -t "can3" -x sudo bash -c "cd ${workspace}/arx_can; ./arx_can3.sh; exec bash;"
-sleep 0.1
-
-gnome-terminal -t "can6" -x sudo bash -c "cd ${workspace}/arx_can; ./arx_can6.sh; exec bash;"
+gnome-terminal -t "can1" -x sudo bash -c "cd ${workspace}/arx_can; ./arx_can0.sh; exec bash;"
